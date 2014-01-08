4e644cd2a9ada97e43561674d80151b055eccb24
jmollica <jmollica@calacademy.org>
2014-01-07 16:00:35 -0800

    dev branch test

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,5 +1,5 @@
 #the git branch you want to use
- set :branch, "dev"
+set :branch, "dev"
 
 role :web, "www-dev" #www-dev                          # Your HTTP server, Apache/etc
 role :app, "www-dev"                          # This may be the same as your `Web` server
--- sites/all/themes/calacademy_zen/template.php
+++ sites/all/themes/calacademy_zen/template.php
@@ -119,8 +119,6 @@ function calacademy_zen_preprocess_maintenance_page(&$variables, $hook) {
 }
 // */
 
-print '<h1>this is dev branch</h1>'; exit;
-
 function calacademy_zen_views_pre_render (&$view){
   switch ($view->name) {
     case 'taxonomy_terms':

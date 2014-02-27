2dcffbc30e918135edd3010d80b71c76debc4245
jmollica <jmollica@calacademy.org>
2014-02-26 17:11:37 -0800

    stg

--- config/deploy/stg.rb
+++ config/deploy/stg.rb
@@ -1,5 +1,5 @@
 #the git branch you want to use
-#set :branch, "stg"
+set :branch, "stg"
 
 role :web, "www2-stg" #www-dev                          # Your HTTP server, Apache/etc
 role :app, "www2-stg"                          # This may be the same as your `Web` server

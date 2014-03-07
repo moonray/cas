ed0af05118812ca858d3d33aefb7f11491e00415
Rob Davidson <rob@custommojo.com>
2014-03-07 09:44:31 -0800

    Added Hero system updates that somehow didnt make it to the new dev server.

---
+++ sites/all/modules/custom/calacademy/js/hero-region--hero-type-toggle.js
@@ -0,0 +1,59 @@
+
+jQuery(document).ready(function($) {
+  // Hide the hero widgets.
+  var selected = $('.field-name-field-hero-type .form-radios input[type=\'radio\']:checked');
+  if (selected.val())
+  {
+    hero_region_reveal_widget(selected.val());
+  }
+  
+  $('.field-name-field-hero-type .form-radios input').on('click', function() {
+    if ($(this).val()) {
+      hero_region_reveal_widget($(this).val());
+    }
+  });
+});
+
+function hero_region_reveal_widget(widget)
+{
+  var className = null;
+  
+  // Hide widgets unless the widget is excluded.
+  if (widget != 'image-standard')
+  {
+    jQuery('.field-name-field-hero-region .field-name-field-image-primary').addClass('element-invisible');
+  }
+  if (widget != 'image-large')
+  {
+    jQuery('.field-name-field-hero-region .field-name-field-image-primary-large').addClass('element-invisible');
+  }
+  if (widget != 'slideshow-standard')
+  {
+    jQuery('.field-name-field-hero-region .field-name-field-hero-slideshow').addClass('element-invisible');
+  }
+  if (widget != 'slideshow-large')
+  {
+    jQuery('.field-name-field-hero-region .field-name-field-hero-slideshow-large').addClass('element-invisible');
+  }
+
+  switch (widget)
+  {
+    case 'image-standard':
+      className = '.field-name-field-image-primary';
+      break;
+      
+    case 'image-large':
+      className = '.field-name-field-image-primary-large';
+      break;
+      
+    case 'slideshow-standard':
+      className = '.field-name-field-hero-slideshow';
+      break;
+      
+    case 'slideshow-large':
+      className = '.field-name-field-hero-slideshow-large';
+      break;
+  }
+
+  jQuery('.field-name-field-hero-region ' + className + '').removeClass('element-invisible');
+}

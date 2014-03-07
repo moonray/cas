849905b696c7685f0c109ecf80b2f4ccd7a0fb6d
Rob Davidson <rob@custommojo.com>
2014-03-07 13:41:40 -0800

    Added PHP error reporting.

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * PHP Error reporting -- Comment out to disable.
+ * - DO NOT DELETE -
+ */
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
 
 /**
  * @file

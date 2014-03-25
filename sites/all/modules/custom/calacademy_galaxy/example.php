3eb48dbd47fa7e5aca561514b473539452a1688e
jmollica <jmollica@calacademy.org>
2014-03-25 12:22:35 -0700

    progress on galaxy module

--- sites/all/modules/custom/calacademy_galaxy/example.php
+++ sites/all/modules/custom/calacademy_galaxy/example.php
@@ -6,7 +6,7 @@ $products = array(
                 "name" => "General Admission",
                 "plu" => "12345678",
                 "url" => "https://wwww.calacademy.org/tickets",
-                "price" => array (
+                "price" => array(
                   "adult edited" => "$129.95",
                   "senior edited" => "$124.95",
                   "student edited" => "$124.95",
@@ -17,7 +17,7 @@ $products = array(
                 "name" => "Nightlife",
                 "plu" => "66666666",
                 "url" => "https://wwww.calacademy.org/nl",
-                "price" => array (
+                "price" => array(
                   "member poo" => "$22210.00",
                   "non-member" => "$44412.00",
                 )

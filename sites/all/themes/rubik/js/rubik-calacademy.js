c22661f14023c3578b3985a3bafe7c44f67b85fa
Joshua Ause <jause@calacademy.org>
2014-04-11 11:28:10 -0700

    hiding and showing hero region image/slideshow standard/large cms options for various content types

--- sites/all/themes/rubik/js/rubik-calacademy.js
+++ sites/all/themes/rubik/js/rubik-calacademy.js
@@ -9,4 +9,29 @@ jQuery(document).ready(function() {
       e.data.dataValue = str.replace(/&nbsp;/g, ' ');
     });
   });
+
+ 	/* Hide "no hero media" option from hero region in cms create/edit for various content types */
+ 	jQuery('#edit-field-hero-region-und-0-field-hero-type-und div:first').css('visibility','hidden');
+ 	jQuery('#edit-field-hero-region-und-0-field-hero-type-und div:first').css('height','0');
+
+ 	/* Hide "standard image" and "standard slideshow" options from hero region in cms create/edit for landing page content */
+ 	jQuery('.node-landing_page-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').closest('div').css('visibility','hidden');
+ 	jQuery('.node-landing_page-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').closest('div').css('height','0');
+ 	jQuery('.node-landing_page-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-standard').closest('div').css('visibility','hidden');
+ 	jQuery('.node-landing_page-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-standard').closest('div').css('height','0');
+
+ 	/*
+ 	* Hide "large image" and "large slideshow" options from hero region in cms create/edit for:
+ 	* base page, content page, event, field trip, lesson plan, event nightlife, and press release content.
+ 	*/
+ 	standardPages = Array('base_page', 'content_page', 'event', 'field_trip', 'lesson_plan', 'event_nightlife', 'press_release');
+ 	var i = 0;
+ 	while (i < standardPages.length) {
+	  jQuery('.node-'+standardPages[i]+'-form #edit-field-hero-region-und-0-field-hero-type-und-image-large').closest('div').css('visibility','hidden');
+ 		jQuery('.node-'+standardPages[i]+'-form #edit-field-hero-region-und-0-field-hero-type-und-image-large').closest('div').css('height','0');
+ 		jQuery('.node-'+standardPages[i]+'-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-large').closest('div').css('visibility','hidden');
+ 		jQuery('.node-'+standardPages[i]+'-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-large').closest('div').css('height','0');
+	  i++;
+	}
+
 });

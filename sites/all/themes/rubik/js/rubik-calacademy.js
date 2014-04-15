b929e24599b3a32eadb20e0de9996df79abf8e9f
Greg Rotter <grotter@calacademy.org>
2014-04-15 08:14:11 -0700

    revisions to rubik-calacademy.js re: hero region option hiding

--- sites/all/themes/rubik/js/rubik-calacademy.js
+++ sites/all/themes/rubik/js/rubik-calacademy.js
@@ -1,42 +1,61 @@
-/*
-* Fix an issue with CKEditor adding a bunch of non-breaking spaces on paste
-* @author Greg Rotter
-*/
-jQuery(document).ready(function() {
-  CKEDITOR.on('instanceReady', function (ev) {
-    ev.editor.on('paste', function (e) {
-      var str = e.data.dataValue;
-      e.data.dataValue = str.replace(/&nbsp;/g, ' ');
-    });
-  });
+jQuery(document).ready(function ($) {
+	/*
+	* Fix an issue with CKEditor adding a bunch of non-breaking spaces on paste
+	* @author Greg Rotter
+	*/
+	CKEDITOR.on('instanceReady', function (ev) {
+		ev.editor.on('paste', function (e) {
+		  var str = e.data.dataValue;
+		  e.data.dataValue = str.replace(/&nbsp;/g, ' ');
+		});
+	});
 
- 	/* Hide "no hero media" option from hero region in cms create/edit for various content types */
- 	jQuery('#edit-field-hero-region-und-0-field-hero-type-und div:first').css('visibility','hidden');
- 	jQuery('#edit-field-hero-region-und-0-field-hero-type-und div:first').css('height','0');
+ 	// Hide "no hero media" option
+ 	$('#edit-field-hero-region-und-0-field-hero-type-und > div:first').hide();
 
- 	/* Hide "standard image" and "standard slideshow" options from hero region in cms create/edit for landing page and exhibit content */
- 	jQuery('.node-landing_page-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').closest('div').css('visibility','hidden');
- 	jQuery('.node-landing_page-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').closest('div').css('height','0');
- 	jQuery('.node-landing_page-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-standard').closest('div').css('visibility','hidden');
- 	jQuery('.node-landing_page-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-standard').closest('div').css('height','0');
+ 	// Hide "standard image" and "standard slideshow"
+ 	var arr = [
+ 		'.node-landing_page-form',
+ 		'.node-exhibit-form'
+ 	];
+ 	
+ 	var i = arr.length;
+ 	
+ 	while (i--) {
+ 		$(arr[i] + ' #edit-field-hero-region-und-0-field-hero-type-und-image-standard').parent().hide();
+ 		$(arr[i] + ' #edit-field-hero-region-und-0-field-hero-type-und-slideshow-standard').parent().hide();
+ 	}
 
- 	jQuery('.node-exhibit-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').closest('div').css('visibility','hidden');
- 	jQuery('.node-exhibit-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').closest('div').css('height','0');
- 	jQuery('.node-exhibit-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-standard').closest('div').css('visibility','hidden');
- 	jQuery('.node-exhibit-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-standard').closest('div').css('height','0');
-
- 	/*
- 	* Hide "large image" and "large slideshow" options from hero region in cms create/edit for:
- 	* base page, content page, event, field trip, lesson plan, event nightlife, and press release content.
- 	*/
- 	var standardPages = new Array('base_page', 'content_page', 'event', 'field_trip', 'lesson_plan', 'event_nightlife', 'press_release');
- 	var i = 0;
- 	while (i < standardPages.length) {
-	  jQuery('.node-'+standardPages[i]+'-form #edit-field-hero-region-und-0-field-hero-type-und-image-large').closest('div').css('visibility','hidden');
- 		jQuery('.node-'+standardPages[i]+'-form #edit-field-hero-region-und-0-field-hero-type-und-image-large').closest('div').css('height','0');
- 		jQuery('.node-'+standardPages[i]+'-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-large').closest('div').css('visibility','hidden');
- 		jQuery('.node-'+standardPages[i]+'-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-large').closest('div').css('height','0');
-	  i++;
+ 	// Hide "large image" and "large slideshow"
+ 	var arr = [
+ 		'base_page',
+ 		'content_page',
+ 		'event',
+ 		'field_trip',
+ 		'lesson_plan',
+ 		'event_nightlife',
+ 		'press_release'
+ 	];
+ 	
+ 	var i = arr.length;
+ 	
+ 	while (i--) {
+	  	$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-image-large').parent().hide();
+		$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-large').parent().hide();
 	}
 
+	// Hide everything except "standard image" (replaces primary image)
+ 	var arr = [
+ 		'person'
+ 	];
+ 	
+ 	var i = arr.length;
+ 	
+ 	while (i--) {
+ 		$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und > div').hide();
+	  	$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').parent().show();
+
+	  	// preselect it as well
+	  	$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').click();
+	}
 });

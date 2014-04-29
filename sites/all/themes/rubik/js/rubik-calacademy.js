135e06507c37e5774927ec9b40050c53272ec268
Rob Davidson <rob@custommojo.com>
2014-04-29 08:18:12 -0700

    Added Raw HTML text format. Enable cropping on Article Section field collection Removed term "afrfica" from Tags vocabulary Added ES Categories vocabulary Enabled YouTube Field module Configured YouTube Field module global settings. Modified Hero Media field collection to contain the following: - Video Settings fieldset -- YouTube Video -- Poster Image Added ES Article content type Added ES Landing Page content type Added Content Collection content type Added Content Collections view Added Category Listings view Added Miscellaneous Content view Added Related Content view Added ES Article page variant Added ES Landing Page page variant

--- sites/all/themes/rubik/js/rubik-calacademy.js
+++ sites/all/themes/rubik/js/rubik-calacademy.js
@@ -32,7 +32,8 @@ jQuery(document).ready(function ($) {
  	// Hide "standard image" and "standard slideshow"
  	var arr = [
  		'landing_page',
- 		'exhibit'
+ 		'exhibit',
+    'es-landing-page'
  	];
  	
  	var i = arr.length;
@@ -40,9 +41,10 @@ jQuery(document).ready(function ($) {
  	while (i--) {
  		$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').parent().hide();
  		$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-standard').parent().hide();
+ 		$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-video-standard').parent().hide();
  	}
 
- 	// Hide "large image" and "large slideshow"
+ 	// Hide "large image", "large slideshow", and "large video"
  	var arr = [
  		'base_page',
  		'content_page',
@@ -50,14 +52,16 @@ jQuery(document).ready(function ($) {
  		'field_trip',
  		'lesson_plan',
  		'event_nightlife',
- 		'press_release'
+ 		'press_release',
+    'explore-science-article'
  	];
  	
  	var i = arr.length;
  	
  	while (i--) {
-	  	$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-image-large').parent().hide();
+    $('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-image-large').parent().hide();
 		$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-slideshow-large').parent().hide();
+		$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-video-large').parent().hide();
 	}
 
 	// Hide everything except "standard image" (replaces primary image)
@@ -69,7 +73,7 @@ jQuery(document).ready(function ($) {
  	
  	while (i--) {
  		$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und > div').hide();
-	  	$('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').parent().show();
+	  $('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').parent().show();
 
 	  	// preselect it as well
 	  	// $('.node-' + arr[i] + '-form #edit-field-hero-region-und-0-field-hero-type-und-image-standard').click();

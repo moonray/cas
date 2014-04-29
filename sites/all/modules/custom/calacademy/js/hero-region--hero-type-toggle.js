cd200f3db8c37088e8dfb0e751f64b505b9ad601
Rob Davidson <rob@custommojo.com>
2014-04-29 12:59:25 -0700

    Ammended: Fixed another issue with the Hero Media field that was causing video settings to alwasy be lost.

--- sites/all/modules/custom/calacademy/js/hero-region--hero-type-toggle.js
+++ sites/all/modules/custom/calacademy/js/hero-region--hero-type-toggle.js
@@ -37,7 +37,6 @@ function hero_region_reveal_widget(widget)
   if (widget != 'video-standard' && widget != 'video-large')
   {
     jQuery('.field-name-field-hero-region .group-video-settings').addClass('element-invisible');
-    jQuery('.field-name-field-hero-region .group-video-settings div').removeClass('element-invisible');
   }
   
   switch (widget)
@@ -63,5 +62,8 @@ function hero_region_reveal_widget(widget)
       className = '.group-video-settings';
       break;
   }
+  // Make sure that the video fields are always visible.
+  jQuery('.field-name-field-hero-region .group-video-settings div').removeClass('element-invisible');
+  // Show the current widget.
   jQuery('.field-name-field-hero-region ' + className + '').removeClass('element-invisible');
 }

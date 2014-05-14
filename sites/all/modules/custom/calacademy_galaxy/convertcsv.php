b9d842c8acc51205db3345ff66dd21f95e40a371
jmollica <jmollica@calacademy.org>
2014-05-14 14:00:41 -0700

    gx module changes

---
+++ sites/all/modules/custom/calacademy_galaxy/convertcsv.php
@@ -0,0 +1,31 @@
+<?php
+
+//header('Content-type: application/json');
+
+// Set your CSV feed
+
+$filename = 'example.csv';
+
+function csv_to_array($filename='', $delimiter=',')
+{
+    if(!file_exists($filename) || !is_readable($filename))
+        return FALSE;
+
+    $header = NULL;
+    $data = array();
+    if (($handle = fopen($filename, 'r')) !== FALSE)
+    {
+        while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+        {
+            if(!$header)
+                $header = $row;
+            else
+                $data[] = array_combine($header, $row);
+        }
+        fclose($handle);
+    }
+    print '<pre>'; print_r($data); print '</pre>';
+}
+
+
+?>

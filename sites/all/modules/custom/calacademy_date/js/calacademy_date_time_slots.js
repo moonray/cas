c5c1e230593c0a55d207fafac12acb87eff9efd1
Joshua Ause <jause@calacademy.org>
2014-06-25 11:49:16 -0700

    pivotal #73413208 rolling back changes to calacademy and calacademy_date modules for field trip time slots bug - fix worked locally but not on stg or dev for some reason

--- sites/all/modules/custom/calacademy_date/js/calacademy_date_time_slots.js
+++ sites/all/modules/custom/calacademy_date/js/calacademy_date_time_slots.js
@@ -5,9 +5,6 @@
 			$("#edit-field-time-slots input.text-full").timePicker({
 				step: 5
 			});
-			$("#edit-field-field-trip-time-slots input.text-full").timePicker({
-				step: 5
-			});
 		}
 	}; 
 

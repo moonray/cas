271df8145805aecca69ee51cc89116f5dc6e1e09
Greg Rotter <grotter@gmail.com>
2014-10-14 10:13:03 -0700

    server-dependent bug fix for ticket #80655042

--- sites/all/themes/calacademy_zen/template.php
+++ sites/all/themes/calacademy_zen/template.php
@@ -250,12 +250,11 @@ function calacademy_zen_menu_link(array $variables) {
 function calacademy_zen_preprocess_html(&$variables, $hook) {
   // chat redirect
   if (request_path() == 'chat') {
-    require('templates/classes/CallCenterHours.php');
+    require(dirname(__FILE__) . '/templates/classes/CallCenterHours.php');
     $foo = new CallCenterHours();
 
     if ($foo->isOpen()) {
-      http_response_code(303);
-      header('location: http://chat.calacademy.org/Code/webchatLogin.php');
+      header('location: http://chat.calacademy.org/Code/webchatLogin.php', true, 301);
       die;
     } else {
       ob_start();

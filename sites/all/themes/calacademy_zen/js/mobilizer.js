67754280037704a4e50e34e1c19865d7736d97da
Jay Mollica <jmollica@calacademy.org>
2014-11-03 10:12:55 -0800

    tryn to fix mobilizer.js for androidz

--- sites/all/themes/calacademy_zen/js/mobilizer.js
+++ sites/all/themes/calacademy_zen/js/mobilizer.js
@@ -1,14 +1,11 @@
+//a script for appending mobile=true parameter to the end of ticketing URLs so that gateway can more easily detect/direct mobile visitors
 jQuery(document).ready(function($) {
 	//check for mobile browser
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		//iterate through anchors
-		$("a").each(function(index) {
-			var str = this.href;
-			//check if it's a ticketing link
-  		if (str.toLowerCase().indexOf("ticketing.calacademy.org") >= 0) {
-  			//append a mobile=true parameter to the end
-  			this.href = str + "&mobile=true";
-  		}
+		$('a[href*="ticketing.calacademy.org"]').each(function () {
+			var orig = $(this).attr('href');
+			$(this).attr('href', orig + '&mobile=true');
 		});
 	}
 });

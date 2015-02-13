f4e11343f0730d87d16f644c2ada62449a7b386c
Greg Rotter <grotter@gmail.com>
2015-02-13 12:25:07 -0800

    more gigamacro refinements

--- sites/all/modules/custom/calacademy_gigamacro/js/calacademy_gigamacro_viewer.js
+++ sites/all/modules/custom/calacademy_gigamacro/js/calacademy_gigamacro_viewer.js
@@ -7,16 +7,22 @@ var CalAcademyGigamacroViewer = function (specimenData) {
 	var _initMap = function (tiles) {
 		// create container
 		$('#content').html('<div id="leaflet-map" />');
-		
+
 		// create map
-		_map = L.map('leaflet-map');
+		_map = L.map('leaflet-map', {
+			zoomControl: false
+		});
+
+		var zoomControl = new L.Control.Zoom({
+			position: 'topright'
+		}).addTo(_map);
 
 		_map.setView([0, 0], 1, {
 			pan: {
 				animate: false
 			}
 		});
-		
+
 		var tilesUrl = '//s3-us-west-1.amazonaws.com/tiles.gigamacro.calacademy.org/';
 		tilesUrl += tiles.toLowerCase() + '/{z}/{x}/{y}.png';
 
@@ -42,10 +48,23 @@ var CalAcademyGigamacroViewer = function (specimenData) {
 		});
 	}
 
+	var _initLegend = function () {
+		$('#content').prepend('<div id="legend"><h1></h1><h2></h2><div class="details"></div></div>');
+
+		$('#legend h1').html(_specimenData.title);
+
+		var s = _getField('field_scientific_name');
+		if (s) $('#legend h2').html(s.safe_value);
+
+		var b = _getField('body');
+		if (b) $('#legend .details').html('<p>' + b.safe_value + '</p>');
+	}
+
 	var _onPinsData = function (data) {
 		_pinsData = data;
 		_initMap(_specimenData.field_gigamacro_specimen.und[0].taxonomy_term.name);
 		_initPins();
+		_initLegend();
 	}
 
 	var _jsonRequest = function (path, myData, onSuccessCallback, onErrorCallback) {
@@ -67,9 +86,23 @@ var CalAcademyGigamacroViewer = function (specimenData) {
 		});
 	}
 
+	var _getField = function (key) {
+		if (_specimenData[key]) {
+			if ($.isArray(_specimenData[key].und)) {
+				if (_specimenData[key].und.length == 1) {
+					return _specimenData[key].und[0];
+				}
+			}
+		}
+
+		return false;
+	}
+
 	this.initialize = function () {
-		var myTid = parseInt(_specimenData.field_gigamacro_specimen.und[0].tid);
-		_jsonRequest('gigamacro-pins', { tid: myTid }, _onPinsData);
+		var spec = _getField('field_gigamacro_specimen');
+		if (!spec) return;
+
+		_jsonRequest('gigamacro-pins', { tid: spec.tid }, _onPinsData);
 	}
 
 	this.initialize();

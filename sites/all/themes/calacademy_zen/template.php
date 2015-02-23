024b4746a1302ad13dadfd212dc5ff3d5baef386
Greg Rotter <grotter@gmail.com>
2015-02-23 14:49:03 -0800

    gigamacro landing page styles

--- sites/all/themes/calacademy_zen/template.php
+++ sites/all/themes/calacademy_zen/template.php
@@ -410,7 +410,7 @@ function calacademy_zen_preprocess_page(&$variables, $hook) {
   drupal_add_css(path_to_theme() . '/css/calacademy/page-search-results.css', $cssOptions);
   drupal_add_css(path_to_theme() . '/css/calacademy/page-simple-form.css', $cssOptions);
   drupal_add_css(path_to_theme() . '/css/calacademy/page-field-trips-landing.css', $cssOptions);
-  drupal_add_css(path_to_theme() . '/css/calacademy/page-lesson-plans-landing-or-press-releases-landing.css', $cssOptions);
+  drupal_add_css(path_to_theme() . '/css/calacademy/page-lesson-plans-landing-and-more.css', $cssOptions);
   drupal_add_css(path_to_theme() . '/css/calacademy/page-user.css', $cssOptions);
 
   drupal_add_css(path_to_theme() . '/css/calacademy/node-type-slideshow-midfeature.css', $cssOptions);

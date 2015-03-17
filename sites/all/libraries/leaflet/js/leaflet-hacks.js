0b3ca740c8c783f2137e7c842bfede451d0be593
Greg Rotter <grotter@gmail.com>
2015-03-17 14:36:26 -0700

    attempting to hack leaflet for pinch / zoom with > 2 touches

--- sites/all/libraries/leaflet/js/leaflet-hacks.js
+++ sites/all/libraries/leaflet/js/leaflet-hacks.js
@@ -22,57 +22,69 @@
     }
 })(jQuery);
 
-// map touchiness
-L.Map.mergeOptions({
-  touchExtend: true
-});
+// @todo
+// more than two pointers for pinch/zoom
+L.Map.TouchZoom.include({
+  _onTouchStart: function (e) {
+    var map = this._map;
 
-L.Map.TouchExtend = L.Handler.extend({
-  initialize: function(map) {
-    this._map = map;
-    this._container = map._container;
-    return this._pane = map._panes.overlayPane;
-  },
-  addHooks: function() {
-    L.DomEvent.on(this._container, 'touchstart', this._onTouchStart, this);
-    L.DomEvent.on(this._container, 'touchend', this._onTouchEnd, this);
-    return L.DomEvent.on(this._container, 'touchmove', this._onTouchMove, this);
-  },
-  removeHooks: function() {
-    L.DomEvent.off(this._container, 'touchstart', this._onTouchStart);
-    L.DomEvent.off(this._container, 'touchend', this._onTouchEnd);
-    return L.DomEvent.off(this._container, 'touchmove', this._onTouchMove);
-  },
-  _onTouchEvent: function(e, type) {
-    var containerPoint, latlng, layerPoint, touch;
-    if (!this._map._loaded) {
-      return;
+    if (!e.touches || e.touches.length < 2 || map._animatingZoom || this._zooming) { return; }
+
+    var p1 = map.mouseEventToLayerPoint(e.touches[0]),
+        p2 = map.mouseEventToLayerPoint(e.touches[e.touches.length - 1]),
+        viewCenter = map._getCenterLayerPoint();
+
+    this._startCenter = p1.add(p2)._divideBy(2);
+    this._startDist = p1.distanceTo(p2);
+
+    this._moved = false;
+    this._zooming = true;
+
+    this._centerOffset = viewCenter.subtract(this._startCenter);
+
+    if (map._panAnim) {
+      map._panAnim.stop();
     }
-    touch = e.touches[0];
-    containerPoint = L.point(touch.clientX, touch.clientY);
-    layerPoint = this._map.containerPointToLayerPoint(containerPoint);
-    latlng = this._map.layerPointToLatLng(layerPoint);
-    return this._map.fire(type, {
-      latlng: latlng,
-      layerPoint: layerPoint,
-      containerPoint: containerPoint,
-      originalEvent: e
-    });
+
+    L.DomEvent
+        .on(document, 'touchmove', this._onTouchMove, this)
+        .on(document, 'touchend', this._onTouchEnd, this);
+
+    L.DomEvent.preventDefault(e);
   },
-  _onTouchStart: function(e) {
-    return this._onTouchEvent(e, 'touchstart');
-  },
-  _onTouchEnd: function(e) {
-    if (!this._map._loaded) {
-      return;
+
+  _onTouchMove: function (e) {
+    var map = this._map;
+
+    if (!e.touches || e.touches.length < 2 || !this._zooming) { return; }
+
+    var p1 = map.mouseEventToLayerPoint(e.touches[0]),
+        p2 = map.mouseEventToLayerPoint(e.touches[e.touches.length - 1]);
+
+    this._scale = p1.distanceTo(p2) / this._startDist;
+    this._delta = p1._add(p2)._divideBy(2)._subtract(this._startCenter);
+
+    if (this._scale === 1) { return; }
+
+    if (!map.options.bounceAtZoomLimits) {
+      if ((map.getZoom() === map.getMinZoom() && this._scale < 1) ||
+          (map.getZoom() === map.getMaxZoom() && this._scale > 1)) { return; }
     }
-    return this._map.fire('touchend', {
-      originalEvent: e
-    });
-  },
-  _onTouchMove: function(e) {
-    return this._onTouchEvent(e, 'touchmove');
+
+    if (!this._moved) {
+      L.DomUtil.addClass(map._mapPane, 'leaflet-touching');
+
+      map
+          .fire('movestart')
+          .fire('zoomstart');
+
+      this._moved = true;
+    }
+
+    L.Util.cancelAnimFrame(this._animRequest);
+    this._animRequest = L.Util.requestAnimFrame(
+            this._updateOnMove, this, true, this._map._container);
+
+    L.DomEvent.preventDefault(e);
   }
 });
-
-L.Map.addInitHook('addHandler', 'touchExtend', L.Map.TouchExtend);

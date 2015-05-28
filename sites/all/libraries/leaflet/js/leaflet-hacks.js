08fd8c2779e13868e96d709e6d9a2395d3d6e4d2
Greg Rotter <grotter@gmail.com>
2015-05-28 12:54:36 -0700

    re-fixing leaflet _onDragEnd bug

--- sites/all/libraries/leaflet/js/leaflet-hacks.js
+++ sites/all/libraries/leaflet/js/leaflet-hacks.js
@@ -1,3 +1,53 @@
+// fixing a bug in _onDragEnd
+L.Map.Drag.include({
+    _onDragEnd: function (e) {
+        var map = this._map,
+            options = map.options,
+            noInertia = true;
+
+            if (jQuery.isArray(this._positions) && jQuery.isArray(this._times)) {
+                noInertia = !options.inertia || this._times.length < 2 || this._positions.length == 0;
+            }
+
+        map.fire('dragend', e);
+
+        if (noInertia) {
+            map.fire('moveend');
+
+        } else {
+
+            var direction = this._lastPos.subtract(this._positions[0]),
+                duration = (this._lastTime - this._times[0]) / 1000,
+                ease = options.easeLinearity,
+
+                speedVector = direction.multiplyBy(ease / duration),
+                speed = speedVector.distanceTo([0, 0]),
+
+                limitedSpeed = Math.min(options.inertiaMaxSpeed, speed),
+                limitedSpeedVector = speedVector.multiplyBy(limitedSpeed / speed),
+
+                decelerationDuration = limitedSpeed / (options.inertiaDeceleration * ease),
+                offset = limitedSpeedVector.multiplyBy(-decelerationDuration / 2).round();
+
+            if (!offset.x && !offset.y) {
+                map.fire('moveend');
+
+            } else {
+                offset = map._limitOffset(offset, map.options.maxBounds);
+
+                L.Util.requestAnimFrame(function () {
+                    map.panBy(offset, {
+                        duration: decelerationDuration,
+                        easeLinearity: ease,
+                        noMoveStart: true,
+                        animate: true
+                    });
+                });
+            }
+        }
+    }
+});
+
 // more than two pointers for pinch/zoom/pan
 L.Map.TouchZoom.include({
   _onTouchStart: function (e) {

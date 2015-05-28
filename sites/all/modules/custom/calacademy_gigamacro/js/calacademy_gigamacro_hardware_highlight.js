cd2a239db664a34dbbea17553c0deb90c9e0ae6d
Greg Rotter <grotter@gmail.com>
2015-05-28 13:16:23 -0700

    added gigamacro hardware highlight lookup

--- sites/all/modules/custom/calacademy_gigamacro/js/calacademy_gigamacro_hardware_highlight.js
+++ sites/all/modules/custom/calacademy_gigamacro/js/calacademy_gigamacro_hardware_highlight.js
@@ -1,14 +1,34 @@
 var CalAcademyGigamacroHardwareHighlight = function () {
 	var $ = jQuery;
 	var _iframe;
-	var _url = '//playground.rotter.org/?key=';
+	
+	var _url = '//localhost/?id=';
+	// var _url = '//playground.rotter.org/?key=';
+
+	var _lookup = {
+		'reset': 66,
+		'egg-grid': 4,
+		'feather-grid': 7,
+		'shell-grid': 3,
+		'comet-moth': 11,
+		'weevils': 10,
+		'glory-bush': 2,
+		'urchin-grid': 0,
+		'scarab-beetle': 8,
+		'grasshopper': 5,
+		'spur-flower': 1,
+		'damselfly': 9,
+		'sunset-moth': 6 
+	};
 
 	this.select = function (val) {
-		_iframe.attr('src', _url + val + '&nocache=' + Math.random());
+		if (typeof(_lookup[val]) == 'undefined') return;
+
+		_iframe.attr('src', _url + _lookup[val] + '&nocache=' + Math.random());
 	}
 
 	this.clear = function () {
-		this.select('0');	
+		this.select('reset');	
 	}
 
 	this.initialize = function () {

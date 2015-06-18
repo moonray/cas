4fc6c62f195b9c357e9f4e8b914bc4d7635ca429
Greg Rotter <grotter@gmail.com>
2015-06-18 10:24:24 -0700

    additional validation for pocket penguins HLS feed

--- sites/all/modules/custom/calacademy_pocket_penguins_feed/YouTubeQuery.php
+++ sites/all/modules/custom/calacademy_pocket_penguins_feed/YouTubeQuery.php
@@ -3,20 +3,34 @@
 	class YouTubeQuery {
 		protected $_endpoint = 'http://youtube.com/get_video_info';
 		protected $_hlsKey = 'hlsvp';
-		protected $_timeoutSecs = 6;
+		protected $_timeoutSeconds = 6;
 
 		public function __construct () {}
 
-		public function getHLS ($id) {
-			$ctx = stream_context_create(
-				array( 
-				    'http' => array( 
-			        	'timeout' => $this->_timeoutSecs 
-			        ) 
-			    ) 
-			); 
+		private function _getCtx () {
+			return stream_context_create(
+				array(
+					'http' => array(
+						'timeout' => $this->_timeoutSeconds
+					)
+				)
+			);
+		}
 
-			$content = file_get_contents($this->_endpoint . '?video_id=' . $id, false, $ctx);
+		private function _isHLSValid ($hlsPath) {
+			$hls = file_get_contents($hlsPath, false, $this->_getCtx());
+			if ($hls === false) return false;
+
+			// @todo
+			// validate stream
+			// var_dump($hls);
+
+			return true;
+		}
+
+		public function getHLS ($id) {
+			// parse data
+			$content = file_get_contents($this->_endpoint . '?video_id=' . $id, false, $this->_getCtx());
 			if ($content === false) return false;
 
 			parse_str($content, $arr);
@@ -25,7 +39,11 @@
 			if (!is_string($arr[$this->_hlsKey])) return false;
 			if (empty($arr[$this->_hlsKey])) return false;
 
-			return $arr[$this->_hlsKey];
+			// check if HLS is valid
+			$hlsPath = $arr[$this->_hlsKey];
+			if (!$this->_isHLSValid($hlsPath)) return false;
+
+			return $hlsPath;
 		}
 	}
 
